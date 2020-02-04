9cd08736b3c54c314060a1bb745efe7a1a0c519b
Maczák Patrik <maczakpatrik@gmail.com>
2020-02-04 12:30:58 +0100

    UserDao update

--- src/main/java/repos/UserDao.java
+++ src/main/java/repos/UserDao.java
@@ -5,6 +5,7 @@ import model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserDao extends JpaRepository {
-    User findByIdWithUser(Long Id);
+public interface UserDao extends JpaRepository<User,Long> {
+    User findByIdWithUser(Long id);
+    User findUserByEmail(String eMail);
 }
