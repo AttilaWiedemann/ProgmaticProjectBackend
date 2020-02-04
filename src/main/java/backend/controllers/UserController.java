4602c4cd109ef62c9584fdad6652a9d3c627d4aa
Horváth Áron <aronhorvath1@gmail.com>
2020-02-04 14:45:55 +0100

    Edit u controller with sample userDTO to get

--- src/main/java/backend/controllers/UserController.java
+++ src/main/java/backend/controllers/UserController.java
@@ -1,6 +1,8 @@
 package backend.controllers;
 
 import backend.dto.UserDto;
+import backend.enums.Gender;
+import backend.enums.Intrest;
 import backend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -8,9 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import backend.services.UserService;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/register")
 public class UserController {
 
     private UserService userService;
@@ -19,16 +21,22 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-/*
-    @RequestMapping(method = RequestMethod.GET)
-    public String showRegister(Model model) {
-        model.addAttribute("user", new UserDto());
-        return "register";
-    }
-    */
 
-    @RequestMapping(method = RequestMethod.POST)
-    public Long register(@Valid @ModelAttribute("user") UserDto user) {
+    @RequestMapping(path = ("/rest/user"), method = RequestMethod.GET)
+    public UserDto sampleUser() {
+        UserDto sampleUser = new UserDto();
+        sampleUser.setEmail("e@mail.cim");
+        sampleUser.setBirthDate(LocalDate.now().minusDays(2));
+        sampleUser.setGender(Gender.FÉRFI);
+        sampleUser.setIntrest(Intrest.NŐ);
+        sampleUser.setName("Ildi bácsi");
+        sampleUser.setPassword("dolgok");
+        return sampleUser;
+    }
+
+
+    @RequestMapping(path = ("/rest/register"), method = RequestMethod.POST)
+    public Long register(@ModelAttribute("user") UserDto user) {
 
         return userService.createUser(user);
     }
