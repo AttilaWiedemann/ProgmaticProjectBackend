5163a50e8b892f3bde493527f925037a6119b8f7
Horváth Áron <aronhorvath1@gmail.com>
2020-02-06 13:24:59 +0100

    Add conversation classes

--- src/main/java/backend/model/Message.java
+++ src/main/java/backend/model/Message.java
@@ -1,11 +1,69 @@
 package backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Message {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String author;
+    private String partner;
+    private String text;
+    @DateTimeFormat(pattern = "yyyy/MMMM/dd HH:mm")
+    private LocalDateTime creationDate;
 
+    @ManyToOne
+    private Conversation conversation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPartner() {
+        return partner;
+    }
+
+    public void setPartner(String partner) {
+        this.partner = partner;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
 }
