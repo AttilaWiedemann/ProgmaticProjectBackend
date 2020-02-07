7f40a9b6a58fd88011d0ae08f7694977b7376ba4
Maczák Patrik <maczakpatrik@gmail.com>
2020-02-07 13:15:55 +0100

    Update UserController, add UserInterest connection

--- src/main/java/backend/controllers/UserController.java
+++ src/main/java/backend/controllers/UserController.java
@@ -1,11 +1,14 @@
 package backend.controllers;
 
 import backend.dto.UserDto;
+import backend.dto.UserInterestDto;
 import backend.dto.UserProfileDto;
 import backend.enums.Gender;
 import backend.enums.Intrest;
 import backend.model.User;
+import backend.model.UserInterest;
 import backend.model.UserProfile;
+import backend.services.UserInterestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +21,12 @@ import java.time.LocalDate;
 public class UserController {
 
     private UserService userService;
+    private UserInterestService userInterestService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService ,UserInterestService userInterestService) {
         this.userService = userService;
+        this.userInterestService = userInterestService;
     }
 
 
@@ -52,5 +57,8 @@ public class UserController {
     public UserProfileDto updateUserProfile(@RequestBody UserProfileDto userProfileDto, Long id) {
         return userService.addOptionalFields(userProfileDto, id);
     }
-
+    @RequestMapping(path =("/rest/user/profile/{id}/interest"),method = RequestMethod.POST)
+    public UserInterestDto addUserInteresttoProfile(@RequestBody UserInterestDto userInterestDto, Long id){
+        return userInterestService.creatUserIterest(userInterestDto,id);
+    }
 }
