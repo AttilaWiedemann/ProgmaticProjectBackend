6f61d5aeeefc0a5c64edb9953eaa12140328cf11
Maczák Patrik <maczakpatrik@gmail.com>
2020-02-07 12:57:43 +0100

    UserInterestService update UserInterest save in UserProfile

--- src/main/java/backend/services/UserInterestService.java
+++ src/main/java/backend/services/UserInterestService.java
@@ -1,8 +1,11 @@
 package backend.services;
 
 import backend.dto.UserInterestDto;
+import backend.model.User;
 import backend.model.UserInterest;
 import backend.repos.UserInterestRepository;
+import backend.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -11,10 +14,20 @@ import javax.persistence.PersistenceContext;
 @Service
 public class UserInterestService {
     private UserInterestRepository userInterestRepository;
+    private UserRepository userRepository;
     @PersistenceContext
     EntityManager em;
 
-    public UserInterestDto creatUserIterest(UserInterestDto userIntDto) {
+    @Autowired
+    public UserInterestService(UserInterestRepository userInterestRepository, UserRepository userRepository) {
+        this.userInterestRepository = userInterestRepository;
+        this.userRepository = userRepository;
+    }
+
+    public UserInterestDto creatUserIterest(UserInterestDto userIntDto, Long id) {
+
+        User user =  userRepository.findUserById(id);
+
         UserInterest userInt = new UserInterest();
         userInt.setBookAndLiterature(userIntDto.isBookAndLiterature());
         userInt.setCulture(userIntDto.isCulture());
@@ -25,7 +38,8 @@ public class UserInterestService {
         userInt.setTechnology(userIntDto.isTechnology());
         userInt.setTravel(userIntDto.isTravel());
         em.persist(userInt);
-
+        user.setUserInterest(userInt);
+        em.persist(user);
         return userIntDto;
     }
 }
