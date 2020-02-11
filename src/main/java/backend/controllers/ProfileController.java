0785347d9fd2e2a50b96c6b0dce5a9175c92d3f0
AttilaWiedemann <attila.wiedemann@gmail.com>
2020-02-11 18:52:20 +0100

    Updates ProfileController

--- src/main/java/backend/controllers/ProfileController.java
+++ src/main/java/backend/controllers/ProfileController.java
@@ -1,13 +1,18 @@
 package backend.controllers;
 
+import backend.dto.ProfileFilterDto;
 import backend.dto.UserProfileWithVisibleFields;
 import backend.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class ProfileController {
     private ProfileService profileService;
 
@@ -16,8 +21,15 @@ public class ProfileController {
         this.profileService = profileService;
     }
 
-    @RequestMapping(path = ("/myProfile"), method = RequestMethod.GET)
+    @RequestMapping(path = ("/rest/myProfile"), method = RequestMethod.GET)
     public UserProfileWithVisibleFields myProfile() {
         return profileService.getProfileInfos();
     }
+
+    @RequestMapping(path = ("/rest/profiles"), method = RequestMethod.GET)
+    public List<UserProfileWithVisibleFields> getUserList(@RequestBody ProfileFilterDto filterDto){
+        //TODO
+        return null;
+    }
+
 }
