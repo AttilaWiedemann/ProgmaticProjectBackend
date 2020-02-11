a453f4fbd9d3bda6f081b488fd178480e54c5cd5
AttilaWiedemann <attila.wiedemann@gmail.com>
2020-02-11 18:36:04 +0100

    Updates profile service
    Image url is commented out to avoid nullpointer Exception

--- src/main/java/backend/services/ProfileService.java
+++ src/main/java/backend/services/ProfileService.java
@@ -1,9 +1,6 @@
 package backend.services;
 
-import backend.dto.ConversationDto;
-import backend.dto.MessageDto;
 import backend.dto.UserProfileWithVisibleFields;
-import backend.model.Conversation;
 import backend.model.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -11,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.time.LocalDate;
 
 @Service
 public class ProfileService {
@@ -21,8 +17,7 @@ public class ProfileService {
 
     @Transactional
     public UserProfileWithVisibleFields getProfileInfos() {
-        String loggedInUserName = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = em.find(User.class, loggedInUserName);
+        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserProfileWithVisibleFields userProfileWithVisibleFields = new UserProfileWithVisibleFields(user.getBirthDate());
         userProfileWithVisibleFields.setAboutMe(user.getUserProfile().getAboutMe());
         userProfileWithVisibleFields.setBodyShape(user.getUserProfile().getBodyShape());
@@ -39,7 +34,7 @@ public class ProfileService {
         userProfileWithVisibleFields.setLikesTravels(user.getUserInterest().isTravels());
         userProfileWithVisibleFields.setSmoking(user.getUserProfile().isSmoking());
         userProfileWithVisibleFields.setName(user.getName());
-        userProfileWithVisibleFields.setImgUrl(user.getProfilePicture().getUrl());
+        //userProfileWithVisibleFields.setImgUrl(user.getProfilePicture().getUrl());
 
         return userProfileWithVisibleFields;
 
