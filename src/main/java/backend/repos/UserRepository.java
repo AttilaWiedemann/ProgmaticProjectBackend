3cd40aa1b0e19364cfd003cbd792cd023a66434e
Horváth Áron <aronhorvath1@gmail.com>
2020-02-18 12:58:37 +0100

    Added exception handling and more logging to messaging functions

--- src/main/java/backend/repos/UserRepository.java
+++ src/main/java/backend/repos/UserRepository.java
@@ -12,4 +12,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
     User findUserById(Long id);
     User findUserByEmail(String eMail);
     List<User> findAllByBirthDateIsLessThanEqualAndBirthDateGreaterThanEqual(LocalDate less, LocalDate more);
+    User findUserByName(String name);
 }
